b6aa3b9fae314a19c807131a4ce4ceff059915c7
andremejia <a19300104@ceti.mx>
2025-09-18 21:12:31 -0600

    Refactorized predictions app

--- backend/predicciones/urls.py
+++ backend/predicciones/urls.py
@@ -1,10 +1,21 @@
-from django.urls import path, include
+# from django.urls import path, include
+# from rest_framework.documentation import include_docs_urls
+# from predicciones import views
+#
+# urlpatterns = [
+#     path('docs/Predicciones/', include_docs_urls(title="Predicciones Api")),
+#     path("api/v1/getCryptos/", views.get_cryptos),
+#     path("api/v1/sendGraphData/", views.send_graph_data),
+#     path("api/v1/sendGraphDataLast/", views.send_graph_data_last),
+# ]
+from django.urls import path
+from .views import get_cryptos, send_series_last, send_close_regression
 from rest_framework.documentation import include_docs_urls
-from predicciones import views
 
 urlpatterns = [
-    path('docs/Predicciones/', include_docs_urls(title="Predicciones Api")),
-    path("api/v1/getCryptos/", views.get_cryptos),
-    path("api/v1/sendGraphData/", views.send_graph_data),
-    path("api/v1/sendGraphDataLast/", views.send_graph_data_last),
+    path('docs/Predicciones/', include_docs_urls(title="Predicciones API")),
+    path('api/v1/cryptos', get_cryptos),
+    path('api/v1/series/last', send_series_last),
+    path('api/v1/series/regression', send_close_regression),
 ]
+
--- backend/simulador/views.py
+++ backend/simulador/views.py
@@ -12,17 +12,21 @@ from .models import PrecioBitcoin, Simulacion, Transaccion
 from .serializers import SimulacionSerializer, TransaccionSerializer, PrecioBitcoinSerializer
 from .ml_model import *
 
+
 class SimuladorView(viewsets.ModelViewSet):
     serializer_class = SimulacionSerializer
     queryset = Simulacion.objects.all()
+
 
 class TransaccionView(viewsets.ModelViewSet):
     serializer_class = TransaccionSerializer
     queryset = Transaccion.objects.all()
 
+
 class PrecioView(viewsets.ModelViewSet):
     serializer_class = PrecioBitcoinSerializer
     queryset = PrecioBitcoin.objects.all()
+
 
 def obtener_precios_ultimos_31_dias():
     url = "https://api.binance.com/api/v3/klines"
@@ -40,10 +44,13 @@ def obtener_precios_ultimos_31_dias():
         precios.append({'fecha': fecha, 'precio': precio})
     return precios
 
+
 def guardar_precios_historicos(simulacion):
     precios = obtener_precios_ultimos_31_dias()
     for precio in precios:
-        PrecioBitcoin.objects.create(simulacion=simulacion, fecha=precio['fecha'], precio=precio['precio'])
+        PrecioBitcoin.objects.create(
+            simulacion=simulacion, fecha=precio['fecha'], precio=precio['precio'])
+
 
 @api_view(['POST'])
 def iniciar_simulacion(request):
@@ -66,6 +73,7 @@ def iniciar_simulacion(request):
     else:
         return Response({'error': 'Debes proporcionar un usuario'}, status=404)
 
+
 @api_view(['POST'])
 def reiniciar_simulacion(request):
     user_id = request.data.get('user')
@@ -87,6 +95,7 @@ def reiniciar_simulacion(request):
     else:
         return Response({'error': 'Debes proporcionar un usuario'}, status=404)
 
+
 @api_view(['GET'])
 def obtener_simulacion(request):
     user_id = request.query_params.get('user')
@@ -102,25 +111,29 @@ def obtener_simulacion(request):
         except Simulacion.DoesNotExist:
             return Response({'error': 'Simulación no encontrada'}, status=404)
     else:
-            return Response({'error': 'Debes proporcionar un usuario'}, status=404)
+        return Response({'error': 'Debes proporcionar un usuario'}, status=404)
 
 
 @api_view(['GET'])
 def obtener_transacciones(request):
     user_id = request.query_params.get('user')
-    if user_id:
-        try:
-            user = User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            return Response({'error': 'Usuario no encontrado'}, status=404)
-        try:
-            transacciones = Transaccion.objects.filter(user=user)
-            serializer = TransaccionSerializer(transacciones, many=True)
-            return Response(serializer.data, status=200)
-        except Exception as e:
-            return Response({'error': str(e)}, status=500)
-    else:
-            return Response({'error': 'Debes proporcionar un usuario'}, status=404)
+    if not user_id:
+        return Response({'error': 'Debes proporcionar un usuario'}, status=404)
+
+    try:
+        user = User.objects.get(id=user_id)
+        simulacion = Simulacion.objects.get(user=user)
+        transacciones = Transaccion.objects.filter(
+            simulacion=simulacion).order_by('fecha')
+        serializer = TransaccionSerializer(transacciones, many=True)
+        return Response(serializer.data, status=200)
+    except User.DoesNotExist:
+        return Response({'error': 'Usuario no encontrado'}, status=404)
+    except Simulacion.DoesNotExist:
+        return Response({'error': 'Simulación no encontrada'}, status=404)
+    except Exception as e:
+        return Response({'error': str(e)}, status=500)
+
 
 @api_view(['GET'])
 def obtener_precios_bitcoin(request):
@@ -132,32 +145,14 @@ def obtener_precios_bitcoin(request):
             return Response({'error': 'Usuario no encontrado'}, status=404)
         try:
             simulacion = Simulacion.objects.get(user=user)
-            precios = PrecioBitcoin.objects.filter(simulacion=simulacion).order_by('fecha')
+            precios = PrecioBitcoin.objects.filter(
+                simulacion=simulacion).order_by('fecha')
             serializer = PrecioBitcoinSerializer(precios, many=True)
             return Response(serializer.data, status=200)
         except Exception as e:
             return Response({'error': str(e)}, status=500)
 
-# @api_view(['POST'])
-# def avanzar_simulacion(request):
-#     user_id = request.data.get('user')
-#     if user_id:
-#         try:
-#             user = User.objects.get(id=user_id)
-#         except User.DoesNotExist:
-#             return Response({'error': 'Usuario no encontrado'}, status=404)
-#         try:
-#             simulacion = Simulacion.objects.get(user=user)
-#             simulacion.fecha += timedelta(days=31)
-#             simulacion.save()
-#             serializer = SimulacionSerializer(simulacion)
-#             return Response(serializer.data, status=200)
-#         except Simulacion.DoesNotExist:
-#             return Response({'error': 'Simulación no encontrada'}, status=404)
-#         except Exception as e:
-#             return Response({'error': str(e)}, status=500)
-#     else:
-#             return Response({'error': 'Debes proporcionar un usuario'}, status=404)
+
 @api_view(['POST'])
 def avanzar_simulacion(request):
     user_id = request.data.get('user')
@@ -169,12 +164,14 @@ def avanzar_simulacion(request):
         try:
             simulacion = Simulacion.objects.get(user=user)
             nueva_fecha = simulacion.fecha + timedelta(days=31)
-            precios_disponibles = PrecioBitcoin.objects.filter(simulacion=simulacion, fecha__gte=simulacion.fecha, fecha__lte=nueva_fecha).count()
+            precios_disponibles = PrecioBitcoin.objects.filter(
+                simulacion=simulacion, fecha__gte=simulacion.fecha, fecha__lte=nueva_fecha).count()
 
             if precios_disponibles < 31:
                 dias_a_predecir = 31 - precios_disponibles
                 model, scaler, look_back = entrenar_modelo_lstm()
-                predecir_precios(model, scaler, look_back, dias_a_predecir=dias_a_predecir, simulacion=simulacion)
+                predecir_precios(
+                    model, scaler, look_back, dias_a_predecir=dias_a_predecir, simulacion=simulacion)
 
             simulacion.fecha = nueva_fecha
             simulacion.save()
@@ -186,6 +183,7 @@ def avanzar_simulacion(request):
             return Response({'error': str(e)}, status=500)
     else:
         return Response({'error': 'Debes proporcionar un usuario'}, status=404)
+
 
 @api_view(['POST'])
 def comprar_bitcoin(request):
@@ -200,14 +198,16 @@ def comprar_bitcoin(request):
             simulacion = Simulacion.objects.get(user=user)
             fecha = request.data.get('fecha')
             fecha_inicial = datetime.strptime(fecha, '%Y-%m-%d').date()
-            precio_actual = PrecioBitcoin.objects.get(simulacion=simulacion, fecha=fecha_inicial)
+            precio_actual = PrecioBitcoin.objects.get(
+                simulacion=simulacion, fecha=fecha_inicial)
             precio = precio_actual.precio
             if simulacion.balance >= cantidad * precio:
                 simulacion.balance -= cantidad * precio
                 simulacion.bitcoins += cantidad
                 simulacion.fecha = fecha_inicial
                 simulacion.save()
-                Transaccion.objects.create(simulacion=simulacion, tipo='compra', cantidad=cantidad, precio=precio)
+                Transaccion.objects.create(
+                    simulacion=simulacion, tipo='compra', cantidad=cantidad, precio=precio)
             else:
                 return Response({'error': 'Balance insuficiente'}, status=400)
             serializer = SimulacionSerializer(simulacion)
@@ -220,6 +220,7 @@ def comprar_bitcoin(request):
             return Response({'error': str(e)}, status=500)
     else:
         return Response({'error': 'Debes proporcionar un usuario'}, status=404)
+
 
 @api_view(['POST'])
 def vender_bitcoin(request):
@@ -234,14 +235,16 @@ def vender_bitcoin(request):
             simulacion = Simulacion.objects.get(user=user)
             fecha = request.data.get('fecha')
             fecha_inicial = datetime.strptime(fecha, '%Y-%m-%d').date()
-            precio_actual = PrecioBitcoin.objects.get(simulacion=simulacion, fecha=fecha_inicial)
+            precio_actual = PrecioBitcoin.objects.get(
+                simulacion=simulacion, fecha=fecha_inicial)
             precio = precio_actual.precio
             if simulacion.bitcoins >= cantidad:
                 simulacion.balance += cantidad * precio
                 simulacion.bitcoins -= cantidad
                 simulacion.fecha = fecha_inicial
                 simulacion.save()
-                Transaccion.objects.create(simulacion=simulacion, tipo='venta', cantidad=cantidad, precio=precio)
+                Transaccion.objects.create(
+                    simulacion=simulacion, tipo='venta', cantidad=cantidad, precio=precio)
             else:
                 return Response({'error': 'Cantidad de bitcoins insuficiente'}, status=400)
             serializer = SimulacionSerializer(simulacion)
@@ -255,6 +258,7 @@ def vender_bitcoin(request):
     else:
         return Response({'error': 'Debes proporcionar un usuario'}, status=404)
 
+
 @api_view(['GET'])
 def predecir_precios_view(request):
     try:
@@ -263,6 +267,7 @@ def predecir_precios_view(request):
         return Response(predicciones, status=200)
     except Exception as e:
         return Response({'error': str(e)}, status=500)
+
 
 @api_view(['PUT'])
 def actualizar_ultima_fecha(request):
@@ -285,7 +290,8 @@ def actualizar_ultima_fecha(request):
         except Exception as e:
             return Response({'error': str(e)}, status=500)
     else:
-            return Response({'error': 'Debes proporcionar un usuario'}, status=404)   
+        return Response({'error': 'Debes proporcionar un usuario'}, status=404)
+
 
 @api_view(['POST'])
 def actualizar_precios_futuros(request):
@@ -298,7 +304,8 @@ def actualizar_precios_futuros(request):
         try:
             simulacion = Simulacion.objects.get(user=user)
             model, scaler, look_back = entrenar_modelo_lstm()
-            predicciones = predecir_precios_bd(model, scaler, look_back, simulacion=simulacion)
+            predicciones = predecir_precios_bd(
+                model, scaler, look_back, simulacion=simulacion)
             fecha = request.data.get('fecha')
             fecha_inicial = datetime.strptime(fecha, '%Y-%m-%d').date()
             solo_fecha_str = fecha_inicial.strftime('%d de %m de %Y')
